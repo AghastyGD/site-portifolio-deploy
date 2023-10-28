602b9c440dfec58352795849b4c528b99f52b5d3
AghastyGD <augustodomingosvasco@gmail.com>
2023-10-28 08:09:28 -0200

    aplicacao_de_animacoes_e_ajustes

--- formulario/static/script.js
+++ formulario/static/script.js
@@ -1,57 +1,58 @@
-// Espera até que todo o DOM seja carregado
-$(document).ready(function() {
-
-  // Adiciona a classe 'sticky' à navbar quando a página é rolada para baixo
-  $(window).scroll(function() {
-    if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky');
-    } else {
-      $('.navbar').removeClass('sticky');
-    }
-    
-    // Adiciona a classe 'show' ao botão de scroll-up-btn quando a página é rolada 500 pixels para baixo
-    if (this.scrollY > 500) {
-      $('.scroll-up-btn').addClass('show');
-    } else {
-      $('.scroll-up-btn').removeClass('show');
-    }
+// Verifica o scroll da janela para adicionar classes com base no deslocamento
+$(document).ready(function () {
+  $(window).scroll(function () {
+      if (this.scrollY > 20) {
+          $('.navbar').addClass("sticky");
+      } else {
+          $('.navbar').removeClass("sticky");
+      }
+      if (this.scrollY > 500) {
+          $('.scroll-up-btn').addClass("show");
+      } else {
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
 
-  // Script de deslize para cima
-  $('.scroll-up-btn').click(function() {
-    $('html').animate({scrollTop: 0});
+  // Botão para voltar ao topo
+  $('.scroll-up-btn').click(function () {
+      $('html').animate({ scrollTop: 0 });
   });
 
-  // Script de animação de digitação
-  var typed = new Typed('.typing', {
-    strings: ['FullStack Web Developer', 'Python/Django Developer', 'Blogueiro', 'Editor de Vídeo', 'Back-End Developer', 'Front-End Developerr'],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+  // Animação de digitação
+  var typed = new Typed(".typing", {
+      strings: ["Desenvolvedor Python", "Editor", "Blogueiro", "Designer Gráfico", "Desenvolvedor Web", "Youtuber", "Freelancer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
   });
 
-  // Script de alternância do menu/navbar
-  $('.menu-btn').click(function() {
-    $('.navbar .menu').toggleClass('active');
-    $('.menu-btn i').toggleClass('active');
-    return false;
+  // Alternar o menu/navbar
+  $('.menu-btn').click(function () {
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+      return false;
   });
+});
 
-  // Script de fechamento automático do menu/navbar quando um link é clicado
-  const menuLinks = document.querySelectorAll('.menu-link');
-  menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      const menuToggle = document.querySelector('.menu-btn');
-      const navbarMenu = document.querySelector('.navbar .menu');
-      const menuBtnIcon = document.querySelector('.menu-btn i');
-      
-      // Define a propriedade 'checked' do menuToggle para 'false'
-      menuToggle.checked = false;
-      
-      // Remove a classe 'active' da navbarMenu e do menuBtnIcon
-      navbarMenu.classList.remove('active');
-      menuBtnIcon.classList.remove('active');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container");
+  container.classList.add("show");
+});
+
+// Observador de interseção para as animações
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('shows');
+      } else {
+          entry.target.classList.remove('shows');
+      }
   });
+});
 
+// Elementos a serem observados
+const elementsToObserve = document.querySelectorAll('h2, .about .about-content .left, .about .about-content .right, .services .serv-content .card, .skills .skills-content .left, .skills .skills-content .right, .contact .contact-content');
+
+elementsToObserve.forEach((element) => {
+  myObserver.observe(element);
 });
